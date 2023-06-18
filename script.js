88fbcff4c79508d56db090f6064a29db8debb98e
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-06-18 17:15:44 -0500

    deleted testing code and variables have different name

--- script.js
+++ script.js
@@ -5,36 +5,18 @@ const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', '
 const numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 const specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '+', '=', '~', '.', ',', ':', ';', '?'];
 
-// let userLength = parseInt(prompt("how many letters?"));
-
-  // let userLength = getSize();
-  // let number = confirm("Numbers?");
-  // let upperChar = confirm("Capitals?");
-  // let lowerChar = confirm("Lower?");
-  // let specials = confirm("Specials?");
-
-  //final password. calling a function
-  //  let finalFinal = generatePlz(userLength,number, upperChar, lowerChar, specials);
-  //  console.log(finalFinal);
-
+//function makes sure that it is within range.
 function getSize(){
-  let characterLength = parseInt(prompt("how many letters?"));
+  let characterLength = parseInt(prompt("How many characters would you like?"));
   if (characterLength < 8 || characterLength > 128){
-    alert("plz at least 8");
+    alert("Password must be at least 8 to 128 characters.");
     return getSize(characterLength);
   }
   return characterLength;
 
 }
-// function optionsExist(){
-//   if (!(number && upperChar && lowerChar && specials)){
-//     alert("Not enough options, please select one.")
 
-//   }
-// }
-
-
- //function called in final password with the arguments
+ //function generates password
 function generatePassword(userLength,number, upperChar, lowerChar, specials) {
 
   
@@ -58,12 +40,13 @@ function generatePassword(userLength,number, upperChar, lowerChar, specials) {
     if (specials) {
       arrayChecklist = arrayChecklist.concat(specialChar);
     }
+
     //picks characters inside the charset array based on the number of characters the user specified.
     for(let i = 0; i < userLength; i++){
       let pickedCharacters = Math.floor(Math.random() * arrayChecklist.length);
       finalPassword += arrayChecklist[pickedCharacters];
     }
-    
+     //checks if at least one is in the password.
     if(
       (number && !finalPassword.match(/[0-9]/)) ||
       (upperChar && !finalPassword.match(/[A-Z]/)) ||
@@ -74,36 +57,6 @@ function generatePassword(userLength,number, upperChar, lowerChar, specials) {
     }
   return finalPassword;
 }
-// var generatePassword = finalFinal
-// console.log(finalFinal);
-// var finalPassword = "";
-//   for (var i = 1; i<= userLength; i++ )
-//      finalPassword += upperCase[i];
-// console.log(finalPassword);
-// let i = Math.floor(Math.random() * upperCase.length);
-// for(let i = 0; i < userLength; i++){
-//   let pickedCharacters = Math.floor(Math.random() * arrayChecklist.length);
-//   finalPassword += arrayChecklist[pickedCharacters];
-// }
-
-
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -111,13 +64,14 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   let userLength = getSize();
-  let number = confirm("Numbers?");
-  let upperChar = confirm("Capitals?");
-  let lowerChar = confirm("Lower?");
-  let specials = confirm("Specials?");
+  let number = confirm(" Would you like numbers?");
+  let upperChar = confirm("Capital letters?");
+  let lowerChar = confirm("Any lowercase?");
+  let specials = confirm("What about special characters?");
 
+// This checks if no options are selected.
   if (!number && !upperChar && !lowerChar && !specials) {
-    alert("Please select at least one character type.");
+    alert("Please select at least one option for your password.");
     return; // Exit the function without generating a password
   }
 
