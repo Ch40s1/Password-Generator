7995c827afa13d52fd59ded987e2ed3cce535619
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-06-20 13:43:37 -0500

    added readme content and made adjustments to comments

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Password-Generator
+This project was made to showcase a password generator that can take user input and generate a password. 
+
+## Link
+https://ch40s1.github.io/My-Portfolio/
+
+### To View
+1. Click the link and try it out  
+ 
+or  
+
+1. Download the files to the system
+2. Open In a any editor (EX: Notepad)
+3. From here drag "index.html" into the browser (chrome preferably)
+
+### To contribute
+1. Copy the SSH or HTTPS url
+2. In the command line make a directory ( git mkdir)
+3. Clone (git clone) and paste the url
+4. Type (code .)
+5. The IDE should allow to submit changes.
+
+
+## Usage
+The password generator can be used to make passwords up to 128 characters.  
+
+![Password-generator](./assets/Screenshot%202023-06-20%20at%201.19.46%20PM.png)
+
+
+## Credits
+
+javaScriptInfo: 
+https://javascript.info/
+
+MDN web docs:
+https://developer.mozilla.org/en-US/docs/Learn
+
+These websites were used as references. 
+
+## Collaborators
+N/A
+
+## License
+MIT license
+
+### Features
+Made with HTML and CSS and javaScript
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment code here
-//Arrays containing all combinations.
+//Arrays containing all characters.
 const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-const specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '+', '=', '~', '.', ',', ':', ';', '?'];
+const specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '+', '=', '~', '.', ',', ':', ';', '?'];
 
 //function makes sure that it is within range.
 function getSize(){
@@ -17,7 +17,7 @@ function getSize(){
 }
 
  //function generates password
-function generatePassword(userLength,number, upperChar, lowerChar, specials) {
+function generatePassword(userLength,number, upperCharacter, lowerCharacter, specials) {
 
   
   //finalPassword is empty for now
@@ -26,35 +26,36 @@ function generatePassword(userLength,number, upperChar, lowerChar, specials) {
   //contains combined arrays after choices
   let arrayChecklist =[];
 
-    //checks to see if the user clicked ok 
-    if (upperChar) {
+    //checks to see if the user clicked ok then merges array to checklist
+    if (upperCharacter) {
       //concat merges 2 arrays: Note it merges the whole!! array.
       arrayChecklist = arrayChecklist.concat(upperCase);
     }
-    if (lowerChar) {
+    if (lowerCharacter) {
       arrayChecklist = arrayChecklist.concat(lowerCase);
     }
     if (number) {
       arrayChecklist = arrayChecklist.concat(numeric);
     }
     if (specials) {
-      arrayChecklist = arrayChecklist.concat(specialChar);
+      arrayChecklist = arrayChecklist.concat(specialCharacter);
     }
 
-    //picks characters inside the charset array based on the number of characters the user specified.
+    //picks characters inside the checklist array based on the number of characters the user specified.
     for(let i = 0; i < userLength; i++){
       let pickedCharacters = Math.floor(Math.random() * arrayChecklist.length);
       finalPassword += arrayChecklist[pickedCharacters];
     }
-     //checks if at least one is in the password.
+     //checks if at least one character is in the password.
     if(
       (number && !finalPassword.match(/[0-9]/)) ||
-      (upperChar && !finalPassword.match(/[A-Z]/)) ||
-      (lowerChar && !finalPassword.match(/[a-z]/)) ||
+      (upperCharacter && !finalPassword.match(/[A-Z]/)) ||
+      (lowerCharacter && !finalPassword.match(/[a-z]/)) ||
       (specials && !finalPassword.match(/[!@#$%^&*-_+=~.,:;?]/))
     ){
-      return generatePassword(userLength,number, upperChar, lowerChar, specials);
+      return generatePassword(userLength,number, upperCharacter, lowerCharacter, specials);
     }
+  //gives back final password 
   return finalPassword;
 }
 
@@ -65,17 +66,17 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   let userLength = getSize();
   let number = confirm(" Would you like numbers?");
-  let upperChar = confirm("Capital letters?");
-  let lowerChar = confirm("Any lowercase?");
+  let upperCharacter = confirm("Capital letters?");
+  let lowerCharacter = confirm("Any lowercase?");
   let specials = confirm("What about special characters?");
 
 // This checks if no options are selected.
-  if (!number && !upperChar && !lowerChar && !specials) {
+  if (!number && !upperCharacter && !lowerCharacter && !specials) {
     alert("Please select at least one option for your password.");
     return; // Exit the function without generating a password
   }
 
-  var password = generatePassword(userLength,number, upperChar, lowerChar, specials);
+  var password = generatePassword(userLength,number, upperCharacter, lowerCharacter, specials);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
